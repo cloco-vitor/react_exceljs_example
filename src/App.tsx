3feba3d2402cb85a6e76f515931f0d6564353794
vitor.campos@cloco.co.jp <vitor.campos@cloco.co.jp>
2022-09-05 10:00:56 +0900

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,36 @@
 import React from 'react';
+import * as Excel from "exceljs";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [fileJson, setFileJson] = React.useState<any>(null);
+
+  const handleChange = (files: FileList | null) => {
+    if (files == null) return
+
+    handleImport(files[0])
+  }
+
+  const handleImport = (file: Blob) => {
+    const wb = new Excel.Workbook();
+    const reader = new FileReader()
+
+    reader.readAsArrayBuffer(file)
+    reader.onload = () => {
+      const buffer = reader.result;
+      if (buffer == null || typeof buffer === 'string') return
+      wb.xlsx.load(buffer).then(workbook => {
+        console.log(workbook, 'workbook instance')
+        workbook.eachSheet((sheet, id) => {
+          sheet.eachRow((row, rowIndex) => {
+            console.log(row.values, rowIndex)
+          })
+        })
+      })
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +38,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <input type="file" onChange={(e) => handleChange(e.target.files)}/>
         <a
           className="App-link"
           href="https://reactjs.org"
